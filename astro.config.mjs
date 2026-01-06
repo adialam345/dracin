1d903d911004658538001f4ba93c49790190f638
adialam345 <adialam345@gmail.com>
2026-01-07 01:13:56 +0700

    feat: add base layout with SEO/PWA meta tags and configure Astro site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dracin.antarixa.qzz.io',
   output: 'server',
   adapter: node({
     mode: 'standalone'
