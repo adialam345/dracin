6518f4dced93d0e6bd48dc2f7a39f268371f13ef
adialam345 <adialam345@gmail.com>
2026-01-10 00:39:05 +0700

    sso

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   devToolbar: {
     enabled: false
   },
