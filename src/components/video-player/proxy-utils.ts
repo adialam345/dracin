3f639533932aa62addc7abe3215b5d1cfa9b2e90
adialam345 <adialam345@gmail.com>
2026-01-11 01:14:59 +0700

    1 video cf

--- src/components/video-player/proxy-utils.ts
+++ src/components/video-player/proxy-utils.ts
@@ -1,8 +1,7 @@
 import { encrypt } from '../../utils/security';
 
 export const VIDEO_PROXIES = [
-    'https://videoproxy.cobaakun116.workers.dev/',
-    'https://video-proxy.mrxnexsus.workers.dev/',
+    'https://videoproxy.cobaakun116.workers.dev/'
 ];
 
 export const PROXY_BASE = VIDEO_PROXIES[0];
